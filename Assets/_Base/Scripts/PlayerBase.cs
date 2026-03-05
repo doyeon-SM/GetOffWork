793def91e3540634ff18375af8becc95388a8de5
doyeon-SM <82075191+doyeon-SM@users.noreply.github.com>
2026-03-05 22:39:49 +0900

    26.03.05
    +PlayerBase
    
    승진 조건 추가
    목표 성과 변수 추가
    엔딩 변수 및 조건 추가

--- Assets/_Base/Scripts/PlayerBase.cs
+++ Assets/_Base/Scripts/PlayerBase.cs
@@ -9,14 +9,33 @@ public class PlayerBase : MonoBehaviour
     public float reliability = 0.0f;
     public int pay = 0;
 
-    enum playerStat
+    
+    [Header("승진 조건")]
+    public int[] promotion = { 30 };
+    [SerializeField]
+    private int promotion_index = 0;
+
+    [SerializeField]
+    private int goalpreformance = 0;
+
+    //플레이어 스탯
+    public enum playerStat
     {
         Kindness,
         Stress,
         Reliability
     }
+    //플레이 엔딩 조건
+    public enum playerEnding
+    {
+        Ending,
+        Unkindness,
+        Stressfull,
+        preformanceless
+    }
 
-    void statPlus(playerStat stat, int amount)
+    //스탯 플러스
+    public void statPlus(playerStat stat, int amount)
     {
         switch (stat)
         {
@@ -25,6 +44,7 @@ public class PlayerBase : MonoBehaviour
                 break;
             case playerStat.Stress:
                 stress += 0.05f * (float)amount;
+                if (stress >= 1.0f) Endingcheck(playerEnding.Stressfull);
                 break;
             case playerStat.Reliability:
                 reliability += 0.05f * (float)amount;
@@ -34,12 +54,14 @@ public class PlayerBase : MonoBehaviour
         }
     }
 
-    void statMinus(playerStat stat, int amount)
+    //스탯 마이너스
+    public void statMinus(playerStat stat, int amount)
     {
         switch (stat)
         {
             case playerStat.Kindness:
                 kindness -= 0.05f * (float)amount;
+                if (kindness <= 0.0f) Endingcheck(playerEnding.Unkindness);
                 break;
             case playerStat.Stress:
                 stress -= 0.05f * (float)amount;
@@ -52,4 +74,57 @@ public class PlayerBase : MonoBehaviour
         }
     }
 
+    //성과 갱신
+    public void setPreformance(int amount)
+    {
+        if(preformance + amount <= 0)
+        {
+            Debug.Log("성과 미달");
+            return;
+        }
+
+        preformance += amount;
+
+        //승진 확인
+        PromotionCheck();
+    }
+
+    //급여 갱신
+    public void setpay(int amount)
+    {
+        if(pay + amount < 0)
+        {
+            Debug.Log("소지금 부족");
+            return;
+        }
+
+        pay += amount;
+    }
+    //승진 확인
+    public void PromotionCheck()
+    {
+        //if : Promotion[Promotion_index] <= preformance ? Promotion_index++ : Promotion_index
+    }
+    //일일 목표 성과 달성 확인
+    public void PreformanceCheck()
+    {
+        //if : goalpreformance > preformance ? Endingcheck(playerEnding.preformanceless) : 목표 갱신
+    }
+    //엔딩 확인
+    public void Endingcheck(playerEnding endingN)
+    {
+        switch(endingN)
+        {
+            case playerEnding.Ending:
+                break;
+            case playerEnding.preformanceless:
+                break;
+            case playerEnding.Stressfull:
+                break;
+            case playerEnding.Unkindness:
+                break;
+            default:
+                break;
+        }
+    }
 }
